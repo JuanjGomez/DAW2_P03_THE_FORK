9089417f05ab44d789e3340351fcaf1cfb6aed34
Aina Orozco Gonzalez <468417.joan23@fje.edu>
2025-02-12 20:14:52 +0100

    welcome view listo estetica

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
